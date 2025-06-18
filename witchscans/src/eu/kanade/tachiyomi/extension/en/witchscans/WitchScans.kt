a1d3739625a0dc45402eea280f899eaee900e599
xdwil <139105949+xdwil@users.noreply.github.com>
2025-06-18 06:21:27 -0600

    .

--- witchscans/src/eu/kanade/tachiyomi/extension/en/witchscans/WitchScans.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.witchscans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class WitchScans : MangaThemesia(
-    "Witch Scans",
-    "https://witchscans.com",
-    "en",
-)
